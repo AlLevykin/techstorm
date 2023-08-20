769ecd0752b03c76402da0ff83f8fc07c3935867
Aleksey Levykin <a.a.levykin@gmail.com>
2023-08-20 11:00:44 +0300

    SPA

---
+++ FluidPumpingSystem/wwwroot/lib/modelTicker.js
@@ -0,0 +1,9 @@
+﻿new Chartist.Line('#pumpRotationalSpeedChart', {
+    labels: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    series: [[100, 120, 180, 200]]
+});
+
+new Chartist.Line('#reservoirLevelChart', {
+    labels: [1, 2, 3, 4],
+    series: [[5, 2, 8, 3]]
+});
